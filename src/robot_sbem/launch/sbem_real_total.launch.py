690977ad43a97144a12ea5082698291f97c5f12f
Enrico Moro <enrimoro003@gmail.com>
2025-04-19 22:36:39 +0200

    small changes in the launch files

--- src/robot_sbem/launch/sbem_real_total.launch.py
+++ src/robot_sbem/launch/sbem_real_total.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.substitutions import PathJoinSubstitution
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
@@ -15,14 +15,16 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    # Check if we're told to use ros2_control
+    use_ros2_control = LaunchConfiguration('use_ros2_control', default='false')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('robot_sbem'))
     xacro_file = os.path.join(pkg_path,'description','robot_sbem.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' use_sim_time:=', use_sim_time])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
