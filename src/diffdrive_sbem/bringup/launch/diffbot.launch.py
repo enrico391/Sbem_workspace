ef0848371a17f26ec85a50c6dada52203a479810
Enrico Moro <enrimoro003@gmail.com>
2025-07-29 13:58:48 +0200

    change name in odometry topic

--- src/diffdrive_sbem/bringup/launch/diffbot.launch.py
+++ src/diffdrive_sbem/bringup/launch/diffbot.launch.py
@@ -98,6 +98,7 @@ def generate_launch_description():
         remappings=[
             ("~/robot_description", "/robot_description"),
             ("/diffbot_base_controller/cmd_vel", "/cmd_vel_stamped"),
+            ("/diffbot_base_controller/odom", "/odom"),
         ],
     )
     robot_state_pub_node = Node(
