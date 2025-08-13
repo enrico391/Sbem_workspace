52efa8662867615be51ba4cc50553c5831bda843
Enrico Moro <enrimoro003@gmail.com>
2025-08-13 16:03:02 +0200

    new handler for encoder values

--- src/diffdrive_sbem/hardware/include/diffdrive_sbem/wheel.hpp
+++ src/diffdrive_sbem/hardware/include/diffdrive_sbem/wheel.hpp
@@ -7,14 +7,19 @@
 
 class Wheel
 {
+    private:
+    double last_enc = 0.0;
+    bool first_read = true;
+    
     public:
 
     std::string name = "";
-    float enc = 0;
+    double enc = 0;  // Changed from float to double for precision
     double cmd = 0;
     double pos = 0;
     double vel = 0;
     double rads_per_count = 0;
+    int counts_per_rev = 0;
 
     Wheel() = default;
 
@@ -24,18 +29,37 @@ class Wheel
     }
 
     
-    void setup(const std::string &wheel_name, int counts_per_rev)
+    void setup(const std::string &wheel_name, int counts_per_revolution)
     {
       name = wheel_name;
-      rads_per_count = (2*M_PI)/counts_per_rev;
+      counts_per_rev = counts_per_revolution;
+      rads_per_count = (2.0 * M_PI) / counts_per_revolution;
     }
 
     double calc_enc_angle()
     {
-      return enc * rads_per_count;
+        if (first_read) {
+            last_enc = enc;
+            first_read = false;
+            return pos; // Return current position, don't change it
+        }
+        
+        // Calculate the difference since last reading
+        double enc_diff = enc - last_enc;
+        
+        // Handle potential encoder overflow/underflow
+        if (enc_diff > (counts_per_rev / 2.0)) {
+            enc_diff -= counts_per_rev;
+        } else if (enc_diff < -(counts_per_rev / 2.0)) {
+            enc_diff += counts_per_rev;
+        }
+        
+        // Update accumulated position
+        pos += enc_diff * rads_per_count;
+        last_enc = enc;
+        
+        return pos;
     }
-
-
 
 };
 
