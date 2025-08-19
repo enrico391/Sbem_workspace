42e81ca7e63166bc052e3b7b138aa14009e8c3d8
Enrico Moro <enrimoro003@gmail.com>
2025-08-19 13:09:58 +0200

    working ros2_control

--- src/diffdrive_sbem/hardware/include/diffdrive_sbem/wheel.hpp
+++ src/diffdrive_sbem/hardware/include/diffdrive_sbem/wheel.hpp
@@ -7,58 +7,31 @@
 
 class Wheel
 {
-    private:
-    double last_enc = 0.0;
-    bool first_read = true;
-    
     public:
 
     std::string name = "";
-    double enc = 0;  // Changed from float to double for precision
     double cmd = 0;
     double pos = 0;
     double vel = 0;
-    double rads_per_count = 0;
-    int counts_per_rev = 0;
+    float turns = 0;
+    int reverse = 1; // 1 for normal, -1 for reverse
 
     Wheel() = default;
 
-    Wheel(const std::string &wheel_name, int counts_per_rev)
+    Wheel(const std::string &wheel_name, int rev)
     {
-      setup(wheel_name, counts_per_rev);
+      setup(wheel_name, rev);
     }
 
-    
-    void setup(const std::string &wheel_name, int counts_per_revolution)
+    void setup(const std::string &wheel_name, int rev)
     {
+      reverse = rev;
       name = wheel_name;
-      counts_per_rev = counts_per_revolution;
-      rads_per_count = (2.0 * M_PI) / counts_per_revolution;
     }
 
-    double calc_enc_angle()
+    double update_from_turns()
     {
-        if (first_read) {
-            last_enc = enc;
-            first_read = false;
-            return pos; // Return current position, don't change it
-        }
-        
-        // Calculate the difference since last reading
-        double enc_diff = enc - last_enc;
-        
-        // Handle potential encoder overflow/underflow
-        if (enc_diff > (counts_per_rev / 2.0)) {
-            enc_diff -= counts_per_rev;
-        } else if (enc_diff < -(counts_per_rev / 2.0)) {
-            enc_diff += counts_per_rev;
-        }
-        
-        // Update accumulated position
-        pos += enc_diff * rads_per_count;
-        last_enc = enc;
-        
-        return pos;
+      return turns * 2 * M_PI * reverse;
     }
 
 };
