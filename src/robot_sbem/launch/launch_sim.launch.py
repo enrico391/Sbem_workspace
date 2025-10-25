2645140fe824f208b328172d625c46d26ce22124
Enrico Moro <enrimoro003@gmail.com>
2025-10-25 15:19:57 +0200

    add latest changes

--- src/robot_sbem/launch/launch_sim.launch.py
+++ src/robot_sbem/launch/launch_sim.launch.py
@@ -12,12 +12,7 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='robot_sbem' #<--- CHANGE ME
+    package_name='robot_sbem'
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
