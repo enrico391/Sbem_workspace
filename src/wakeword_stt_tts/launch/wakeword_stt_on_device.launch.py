17a0cda7f15c2e09f855e056697a8c5bfefaf1cd
Enrico Moro <enrimoro003@gmail.com>
2025-11-02 15:45:05 +0100

    small changes to launch files

--- src/wakeword_stt_tts/launch/wakeword_stt_on_device.launch.py
+++ src/wakeword_stt_tts/launch/wakeword_stt_on_device.launch.py
@@ -2,19 +2,20 @@ from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    
     wake_record_mic_node = Node(
             package="wakeword_stt_tts",
             executable="wakeword_stt_whisper_server",
             name="wakeword_stt_whisper_server",
             output="screen",
             parameters=[{
-                "channels": 1,
-                "rate": 44100,
-                "device": 0,
+                "channels": LaunchConfiguration('channels'),
+                "rate": LaunchConfiguration('rate'),
+                "device": LaunchConfiguration('device'),
                 "on_device": True                
             }]
         )
@@ -27,6 +28,18 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'channels',
+            default_value='1',
+            description='Number of audio channels'),
+        DeclareLaunchArgument(
+            'rate',
+            default_value='44100',
+            description='Audio sampling rate'),
+        DeclareLaunchArgument(
+            'device',
+            default_value='25',
+            description='Audio input device index'),
         wake_record_mic_node,
         stt_server
     ])
