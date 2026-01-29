fb75121be52f394ad612548fef7f025a05002354
Enrico <enrimoro003@gmail.com>
2026-01-29 10:37:07 +0100

    new branch for jetson with isaac_ros

--- src/robot_sbem/launch/sbem_real_total.launch.py
+++ src/robot_sbem/launch/sbem_real_total.launch.py
@@ -34,58 +34,58 @@ def generate_launch_description():
             'docking_sbem.launch.py'))
     )
 
-    ros_bridge_server = Node(
-        package='rosbridge_server',
-        executable='rosbridge_websocket',
-        name='rosbridge_websocket',
-        output='screen',
-    )
+    # ros_bridge_server = Node(
+    #     package='rosbridge_server',
+    #     executable='rosbridge_websocket',
+    #     name='rosbridge_websocket',
+    #     output='screen',
+    # )
 
-    # Create a node image republisher node
-    node_image_republisher = Node(
-        package='image_transport',
-        executable='republish',
-        output='screen',
-        name='image_republisher_compressed',
-        arguments=['compressed', 'raw'],
-        remappings=[
-            ('in/compressed', '/image_raw/compressed'),
-            ('out', '/image')
-        ],
-    )
+    # # Create a node image republisher node
+    # node_image_republisher = Node(
+    #     package='image_transport',
+    #     executable='republish',
+    #     output='screen',
+    #     name='image_republisher_compressed',
+    #     arguments=['compressed', 'raw'],
+    #     remappings=[
+    #         ('in/compressed', '/image_raw/compressed'),
+    #         ('out', '/image')
+    #     ],
+    # )
 
-    apriltag_node = Node(
-            package='apriltag_ros',
-            executable='apriltag_node',
-            name='apriltag_node',
-            output='screen',
-            parameters=[{
-                'tag_family': '36h11',
-                'publish_tf': True,
-                'tf_prefix': 'tag_detections',
-                'camera_frame_id': 'camera_link',
-                'tag_size': 0.075,  # Size of the tag in meters
-            }],
-            remappings=[
-                ('image_rect', '/image'),
-                ('camera_info', '/camera_info'),
-            ],
-        )
+    # apriltag_node = Node(
+    #         package='apriltag_ros',
+    #         executable='apriltag_node',
+    #         name='apriltag_node',
+    #         output='screen',
+    #         parameters=[{
+    #             'tag_family': '36h11',
+    #             'publish_tf': True,
+    #             'tf_prefix': 'tag_detections',
+    #             'camera_frame_id': 'camera_link',
+    #             'tag_size': 0.075,  # Size of the tag in meters
+    #         }],
+    #         remappings=[
+    #             ('image_rect', '/image'),
+    #             ('camera_info', '/camera_info'),
+    #         ],
+    #     )
 
-    server_stt = Node(
-        package='sbem_speaking',
-        executable='stt_socket_server.py',
-        name='audio_transcription_server',
-        output='screen',
-    )
+    # server_stt = Node(
+    #     package='sbem_speaking',
+    #     executable='stt_socket_server.py',
+    #     name='audio_transcription_server',
+    #     output='screen',
+    # )
     
     # Launch!
     return LaunchDescription([
         localization_node,
         navigation_node,
         docking_node,
-        node_image_republisher,
-        apriltag_node,
-        ros_bridge_server,
-        server_stt,
+        # node_image_republisher,
+        # apriltag_node,
+        # ros_bridge_server,
+        # server_stt,
     ])
